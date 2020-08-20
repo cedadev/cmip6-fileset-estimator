f30cbad91070977e9a2e0ac9dbacfde19cd9da36
Francesca Eggleton <francesca.eggleton@stfc.ac.uk>
2020-08-20 10:05:40 +0100

    adding ingest wrappers

--- submit_lotus.sh
+++ submit_lotus.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Will only work when run on Francesca's home directory and in the correct directory (relative to Fran's env) - this wrapper is run on Fran's CRON machine, this is for reference only
+#for ingest control
 
-source $HOME/.bash_profile
-BASEDIR=/home/users/eggleton/CEDA/CMIP6/cmip6-fileset-estimator/
+#source $HOME/.bash_profile
+BASEDIR=/home/badc/software/datasets/cmip6/cmip6-fileset-estimator/
 
 cd ${BASEDIR}
 cd scripts/
 
-sbatch -o ../lotus_logs/slurm-%j.out -t 6:00 ./fileset_table_creator_wrapper.sh
+sbatch -o /home/badc/software/datasets/cmip6/cmip6-fileset-estimator/lotus_logs/slurm-%j.out -t 6:00 ./fileset_table_creator_wrapper.sh
 
 
